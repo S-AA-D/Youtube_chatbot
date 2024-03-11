c128eb9bade23e62ec96523a9e67103a28fc5a60
S-AA-D <117470483+S-AA-D@users.noreply.github.com>
2024-03-12 01:33:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from chatbot import YoutubeChatBot
+
+chatbot = YoutubeChatBot(youtube_url="ENTER THE VIDEO URL HERE" ) # If this was your first interact with the model it will transcript the video and this process may take several minutes depending on the video long
+
 while True:
   question = input("Enter your question : ")
   print(chatbot.ask(question))
